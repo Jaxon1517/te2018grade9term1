d9b4755951d79287cd1a57d8f4f69446b6047975
Jaxon1517 <james.hartley1517>
2018-10-06 15:45:41 -0400

    Completed challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,27 @@
 // Example
-let message = "Hello World!";
-console.log(message);
+const READLINE = require("readline-sync")
 
 // Type your code below!
+let grade = 9;
+let myAge = 14;
+let numberOfPets = 1;
+let favoriteAnimal = "octopus";
+let burough = "Queens";
+let serviceCarrier = "TMobile";
+let favoriteColorYellow = false;
+let playsSoccer = true;
+let satScore;
+
+console.log(myAge);
+console.log(grade);
+console.log(numberOfPets);
+console.log(favoriteAnimal);
+console.log(burough);
+console.log(serviceCarrier);
+console.log(favoriteColorYellow);
+console.log(playsSoccer);
+console.log(satScore);
+
+
+let userInput=READLINE.question('hello')
+console.log('You Typed: ${userInput}')
