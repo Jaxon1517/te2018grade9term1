90211a72ebcfed471f0588904f2199a0ce5004f6
Jaxon1517 <james.hartley1517>
2018-10-20 12:21:35 -0400

    Finished challenge #3

---
+++ Object.js
@@ -0,0 +1,18 @@
+let villan = {
+firstName: "Pop",
+lastName: "Francisco",
+greatestCrime:"caused 1,000 account breaches instantly",
+catchphrase:"nothing is safe"
+};
+
+let hacker = {
+firstName:"Dimitri",
+lastName: "Smith",
+nickName:"Sinker",
+catchphrase:"I got u",
+archenemy:"Pop Francisco"
+};
+console.log(`This is ${hacker.firstName} ${hacker.lastName},
+Codename:${hacker.nickName}. She will be working against ${villan.firstName} ${villan.lastName}. Would you like to say anything? 
+... 
+'I got u bb'`)
