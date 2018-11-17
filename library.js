a5b4974d53a655099670398bcbf7ffa866393722
Jaxon1517 <james.hartley1517>
2018-11-17 10:11:02 -0500

    Finished Library.js

--- library.js
+++ library.js
@@ -1,1 +1,38 @@
-l
+let library ={
+	name: "Crybrary",
+	numShelves: 26
+	shelves:  ("A""B""C""D""E""F""G""H""I""J""K""L""M""N""O""P""Q""R""S""T""U""V""W""X""Y")
+
+}
+let shelves ={
+	A:""
+	B:""
+	C:""
+	D:""
+	E:""
+	F:""
+	G:""
+	H:""
+	I:""
+	J:""
+	K:""
+	L:""
+	M:""
+	N:""
+	O:""
+	P:""
+	Q:""
+	R:""
+	S:""
+	T:""
+	U:""
+	V:""
+	W:""
+	X:""
+	Y:""
+	Z:""
+}
+let shelf.B ={
+numBooks: 30
+books: "Tuck Everlasting, by Natalie Babbitt"
+}
